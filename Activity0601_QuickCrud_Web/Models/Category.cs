b025623091d56580566766f3cab3c7ceccfba2af
Tramall <burykin-2001@list.ru>
2021-03-15 19:16:58 +0300

    Добавьте файлы проекта.

---
+++ Activity0601_QuickCrud_Web/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity0601_QuickCrud_Web.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
